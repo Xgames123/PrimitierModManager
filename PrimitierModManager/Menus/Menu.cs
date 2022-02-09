db694bff665837d801055d6dfe31122c233cfbdb
Xgames <Sibe@SIBE-GAMING>
2022-02-09 18:32:52 +0100

    Add project files.

---
+++ PrimitierModManager/Menus/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PrimitierModManager
+{
+	public interface IMenu
+	{
+		public void OnOpen() { }
+		public void OnClose() { }
+
+	}
+}
